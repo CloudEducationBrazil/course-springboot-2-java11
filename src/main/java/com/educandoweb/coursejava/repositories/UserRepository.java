c3139d7dfcf0a83ac22760cb55a6c232da99a7c0
Heleno Cardoso <helenocardosofilho@gmail.com>
2021-12-27 19:33:25 -0300

    JPA repository (Interface - sub framework); dependency injection (containner), database seeding

---
+++ src/main/java/com/educandoweb/coursejava/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.educandoweb.coursejava.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.coursejava.entities.User;
+
+// JPA repository Interface) (sub framework); dependency injection (container), database seeding (popular dados)
+public interface UserRepository extends JpaRepository<User, Long>  {
+
+}
