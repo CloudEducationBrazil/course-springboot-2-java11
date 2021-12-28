53fc8786f2c3def445c1b64a225def9f9f3e6027
Heleno Cardoso <helenocardosofilho@gmail.com>
2021-12-28 12:50:01 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/coursejava/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/coursejava/repositories/UserRepository.java
@@ -6,5 +6,5 @@ import com.educandoweb.coursejava.entities.User;
 
 // JPA repository Interface) (sub framework); dependency injection (container), database seeding (popular dados)
 public interface UserRepository extends JpaRepository<User, Long>  {
-
+  
 }
---
+++ src/main/java/com/educandoweb/coursejava/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.coursejava.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.coursejava.entities.User;
+import com.educandoweb.coursejava.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	// Para injeção de dependência seja feita pelo Spring Boot de forma transparente (automática)
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id){
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
