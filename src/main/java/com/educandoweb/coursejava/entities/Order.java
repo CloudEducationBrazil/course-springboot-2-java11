0a631229cebc10a272fa402d0ae75948a1b6d3c0
Heleno Cardoso <helenocardosofilho@gmail.com>
2021-12-28 20:09:24 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/coursejava/entities/Order.java
+++ src/main/java/com/educandoweb/coursejava/entities/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.educandoweb.coursejava.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 //import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -19,25 +20,31 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @Table(name = "tb_order")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
-	//@JsonIgnore Está habilitado na class User (Somente em uma class)
+
+	// private OrderStatus orderStatus;
+	private Integer orderStatus;
+
+	// @JsonIgnore Está habilitado na class User (Somente em uma class)
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client; // Associação - Diagrama de Classe DER
 
-	public Order(){}
+	public Order() {
+	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		//this.orderStatus = orderStatus;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -55,6 +62,18 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		// return orderStatus;
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		// this.orderStatus = orderStatus;
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
@@ -92,6 +111,6 @@ public class Order implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Order [id=" + id + ", moment=" + moment + ", client=" + client + "]";
+		return "Order [id=" + id + ", moment=" + moment + ", orderStatus=" + orderStatus + ", client=" + client + "]";
 	}
 }
