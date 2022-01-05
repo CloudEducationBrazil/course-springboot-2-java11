618b054114b8c7541ddb8b18d1c46300edef317c
Heleno Cardoso <helenocardosofilho@gmail.com>
2022-01-05 15:59:47 -0300

    User delete

--- src/main/java/com/educandoweb/coursejava/resources/UserResource.java
+++ src/main/java/com/educandoweb/coursejava/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,36 +19,48 @@ import com.educandoweb.coursejava.services.UserService;
 
 // Anottations
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list); 
+		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
-		return ResponseEntity.ok().body(obj); 
+		return ResponseEntity.ok().body(obj);
 	}
-	
-	//@GetMapping
-	//public ResponseEntity<User> findAll(){
-		// Mocar dados
-		//User u = new User(1L, "Julia Maria", "juju@gmail.com", "88776655", "123");
-		//return ResponseEntity.ok().body(u);
-	//}		
-	
+
+	// @GetMapping
+	// public ResponseEntity<User> findAll(){
+	// Mocar dados
+	// User u = new User(1L, "Julia Maria", "juju@gmail.com", "88776655", "123");
+	// return ResponseEntity.ok().body(u);
+	// }
+
 	@PostMapping()
-	public ResponseEntity<User> insert(@RequestBody User obj){
+	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
-				buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).body(obj); 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj); // Código Http: 201
+	}
+
+/*	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build(); // Resposta sem corpo(body). Código Http: 204
+	} */
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<User> delete(@PathVariable Long id) {
+		User obj = service.findById(id);
+		service.delete(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
