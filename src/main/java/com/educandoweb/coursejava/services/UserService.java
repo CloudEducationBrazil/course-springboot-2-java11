d4cf5116257740b265e0bab1075bc6efb03d779c
Heleno Cardoso <helenocardosofilho@gmail.com>
2022-01-21 11:03:43 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/coursejava/services/UserService.java
+++ src/main/java/com/educandoweb/coursejava/services/UserService.java
@@ -3,6 +3,8 @@ package com.educandoweb.coursejava.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -20,47 +22,53 @@ public class UserService {
 	// Injeção de dependência - Dependency Injection
 	// Feita pelo Spring Boot de forma transparente (automática)
 	// Padrão de desenvolvimento de sistemas
-	// para manter baixo o nível de acoplamento entre diferentes módulos de um sistema
-	
+	// para manter baixo o nível de acoplamento entre diferentes módulos de um
+	// sistema
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
-	public User findById(Long id){
+
+	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		//return obj.get();
-		return obj.orElseThrow( () -> new ResourceNotFoundException(id));
+		// return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
-	public User insert(User obj){
+
+	public User insert(User obj) {
 		return repository.save(obj);
 	}
 
-	public void delete(Long id){
+	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		} catch (DataIntegrityViolationException e ) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		} catch (RuntimeException e) {
 			e.printStackTrace(); // Verificar o tipo da mensagem de erro
 		}
 	}
 
-	public User update(Long id, User obj){
-		User entity = repository.getById(id); // getOne(id); prepara o objeto monitorado
-		updateData(entity, obj);
-		return repository.save(entity);
-	}	
+	public User update(Long id, User obj) {
+		try {
+			User entity = repository.getById(id); // getOne(id); prepara o objeto monitorado
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			//e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		}
+	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		//entity.setPassword(obj.getPassword()); // Por conta de segurança
+		// entity.setPassword(obj.getPassword()); // Por conta de segurança
 	}
 }
