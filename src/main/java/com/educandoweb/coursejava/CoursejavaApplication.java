75be41814167ca19d7d1240bd4de4bdb378c89a4
Heleno Cardoso <helenocardosofilho@gmail.com>
2022-01-22 14:10:54 -0300

    Deploy app to Heroku ajuste

--- src/main/java/com/educandoweb/coursejava/CoursejavaApplication.java
+++ src/main/java/com/educandoweb/coursejava/CoursejavaApplication.java
@@ -3,6 +3,7 @@ package com.educandoweb.coursejava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// https://app-javasb-postgre.herokuapp.com/
 @SpringBootApplication
 public class CoursejavaApplication {
 
